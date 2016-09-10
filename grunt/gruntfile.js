dc384909e8f9f4ca29875a3cc199a55a245059ee
alexcaiza <alex.raul.caiza.morillo@gmail.com>
2016-09-10 16:09:16 -0500

    grunpjs

--- grunt/gruntfile.js
+++ grunt/gruntfile.js
@@ -6,11 +6,25 @@ module.exports = function(grunt) {
 	  options: {
         reporterOutput: "",
       }
-    }
+    },
+	concat: {
+		dist: {
+			src: ['scripts.js','scripts1.js','scripts2.js'],
+			dest: 'unidos.js'
+		}
+	},
+	uglify: {
+		dist: {
+			src: 'unidos.js',
+			dest: 'build/unidos.min.js'
+		}
+	}
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
 
-  grunt.registerTask('default', ['jshint']);
+  grunt.registerTask('default', ['jshint','concat','uglify']);
 
 };
---
+++ grunt/unidos.js
@@ -0,0 +1,66 @@
+function AlumnosController($scope) 
+{
+	
+	$scope.alumnos = 
+	[
+		{
+			nombre: "Alex Caiza",
+			telefono: "11111",
+			curso: "2"
+		},
+		{
+			nombre: "Maria Endara",
+			telefono: "554545",
+			curso: "1"
+		},
+		{
+			nombre: "Sandra Salazar",
+			telefono: "344434",
+			curso: "3"
+		},
+		{
+			nombre: "Jose Cera",
+			telefono: "22222",
+			curso: "2"
+		},
+		{
+			nombre: "Raul Caiza",
+			telefono: "11111",
+			curso: "7"
+		}
+	];
+
+	$scope.Save = function() {
+		$scope.alumnos.push(
+			{
+				nombre: $scope.nuevoAlumno.nombre,
+				telefono: $scope.nuevoAlumno.telefono,
+				curso: $scope.nuevoAlumno.curso,
+			}
+		);
+		$scope.nuevoAlumno = {};
+		$scope.formVisibility = false;
+		
+		console.log($scope.formVisibility);
+	};
+
+	$scope.formVisibility = false;
+
+	$scope.ShowForm = function() {
+		$scope.formVisibility = true;
+		console.log($scope.formVisibility);
+	};
+
+	$scope.Cancel = function() {
+		$scope.formVisibility = false;
+		console.log($scope.formVisibility);
+		
+		$scope.nuevoAlumno = {};
+	};
+}
+function script1Funcition() 
+{
+}
+function script2Funcition() 
+{
+}
