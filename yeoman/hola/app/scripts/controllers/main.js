3bb01dc9f2e482254378d716fb3f19a7b8de8f65
alexcaiza <alex.raul.caiza.morillo@gmail.com>
2016-09-11 21:32:20 -0500

    grunt test

---
+++ yeoman/hola/app/scripts/controllers/main.js
@@ -0,0 +1,16 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name holaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the holaApp
+ */
+angular.module('holaApp').controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
